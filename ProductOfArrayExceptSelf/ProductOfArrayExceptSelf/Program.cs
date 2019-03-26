33488e51e7e4b8a5b8fa561a3b01a308c7cff3e6
Talent30 <jon.sun30@gmail.com>
2019-03-27 09:56:19 +1300

    Add files via upload

---
+++ ProductOfArrayExceptSelf/ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] a = new[] {1, 2, 3, 4};
+            OutPut(a);
+        }
+
+        public static void OutPut(int[] a)
+        {
+            int[] product = new int[a.Length];
+            for (int i = 0; i < product.Length; i++)
+            {
+                product[i] = 1;
+            }
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                product[i] = a[i - 1] * product[i - 1];
+            }
+
+            int temp = 1;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                product[i] = product[i] * temp;
+                temp *= a[i];
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(product[i]);
+            }
+        }
+    }
+}
